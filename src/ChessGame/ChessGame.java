51ccc7e6aad2b9fb8fadc2f3aed625c36ce6164f
Owan Lazic <ocjlaz@gmail.com>
2024-11-26 21:49:12 +1300

    added basic logic to control the flow of the game

--- src/ChessGame/ChessGame.java
+++ src/ChessGame/ChessGame.java
@@ -2,4 +2,39 @@ package ChessGame;
 
 public class ChessGame
 {
+    ChessBoard board;
+    private ChessPiece selectedPiece = null;
+    private Boolean whitesTurn = true;
+    public ChessGame()
+    {
+        board = ChessBoard.newGame();
+    }
+
+    public void selectPiece(int x, int y)
+    {
+        if(selectedPiece.getPossibleMoves(board).contains(new Integer[]{x, y}))
+            return;
+
+        selectedPiece = board.board()[x][y];
+    }
+
+    public void selectPiece(ChessPiece piece)
+    {
+        selectedPiece = piece;
+    }
+
+    public void moveSelectedPiece(int desiredX, int desiredY)
+    {
+        if(!selectedPiece.getPossibleMoves(board).contains(new Integer[]{desiredX, desiredY})) //does not contain the desired move as a valid move
+        {
+            selectedPiece = null;
+            return;
+        }
+
+        board = board.newBoardWithMove(selectedPiece.x(), selectedPiece.y(), desiredX, desiredY);
+        whitesTurn = !whitesTurn;
+        selectedPiece = null;
+    }
+
+
 }
